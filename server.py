d28d42724cf2670f99ce74c52a371c124c083851
Cary Carter <ccarterdev@gmail.com>
2023-05-24 19:42:55 -0500

    web ui upload and download

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 # server.py
 #!/usr/bin/env python3
-from flask import Flask, send_from_directory, request
-import os
+from flask import Flask, send_file, request
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """Render the web UI."""
+    return send_file('index.html')
 
 @app.route('/download', methods=['GET'])
 def download():
     """Provide the 50MB file for download."""
-    return send_from_directory(os.getcwd(), '50mb.data')
+    return send_file('50mb.data', as_attachment=True)
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -20,4 +24,4 @@ def upload():
         return {'message': 'No file received'}, 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
